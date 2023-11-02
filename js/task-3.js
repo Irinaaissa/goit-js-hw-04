8a6a796b888b442e6dc62f062d024954dd9f8c58
Irinaaissa <irinaalisa7506@gmail.com>
2023-11-02 20:14:28 +0200

    3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,20 @@
+
 const profile = {
-    username: "Jacob",
+  username: "Jacob",
   playTime: 300,
 };
 
+profile.changeUsername = function(newName) {
+  this.username = newName;
+}
+
+profile.updatePlayTime = function(hours) {
+  this.playTime += hours;
+}
+
+profile.getInfo = function() {
+  return `${this.username} has ${this.playTime} active hours!`;
+}
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
 profile.changeUsername("Marco");
